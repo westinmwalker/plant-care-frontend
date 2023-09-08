b780b58cbffd0f0234dd9eba8278cad191eb2f8a
Westin Walker <westinwalker1988@gmail.com>
2023-09-08 12:42:41 -0600

    Places objects in PlantsIndex in descending # order

--- src/PlantsIndex.jsx
+++ src/PlantsIndex.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable react/prop-types */
 export function PlantsIndex(props) {
+  const numAscending = [...props.plants].sort((a, b) => a.id - b.id);
+  console.log(numAscending);
+
   return (
     <div>
       <h1>All Plants</h1>
-      {props.plants.map((plant) => (
+      {numAscending.map((plant) => (
         <div key={plant.id}>
-          <h2>{plant.name}</h2>
+          <h2>{[plant.id, ". ", plant.name]}</h2>
           <img src={plant.image_url} />
           <button onClick={() => props.onShowPlant(plant)}>More information</button>
           <br />
